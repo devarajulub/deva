412d039025c2b60cf2ee950a2d5e4da81df97247
devarajulub <33396134+devarajulub@users.noreply.github.com>
2017-11-05 20:44:12 +0530

    Add files via upload

---
+++ Exceptions/src/deva/com/Simple.java
@@ -0,0 +1,26 @@
+package deva.com;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+try
+{
+	System.out.println("try");
+	System.exit(0);
+}
+catch(Exception e)
+{
+	System.out.println("catch");
+	
+	
+}
+finally
+{
+	
+	System.out.println("finally");
+	
+}
+	}
+
+}
